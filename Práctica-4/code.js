5bab0a9a760897fcc609e5afdefb867e2163b6d7
Pablo400 <alu0101311135@ull.edu.es>
2022-03-31 23:48:45 +0100

    Cambios Menores

--- Práctica-4/code.js
+++ Práctica-4/code.js
@@ -9,20 +9,29 @@ function calculateMoney() {
   const clientInformation = '{"dni":"none", "birthday":"none", "accountNumber":"none", "productsNumber":0, "discount":0, "paymentMethod":"none"}'
   const object = JSON.parse(clientInformation);
   
+  var totalPrice = 0;
   var price = 0;
+  var i = 1;
+  var priceProduct = [];
   object.dni = prompt("Introduzca su DNI", "XXXXXXXXX");
   object.birthday = prompt("Introduzca su cumpleaños", "1/1/22");
   object.accountNumber = prompt("Introduzca su número de cuenta", "XXXX-XXXX-XXXX-XXXX");
-  object.productsNumber = prompt("Introduzca algún texto", 2);
+  object.productsNumber = prompt("Introduzca algún texto", 0);
+
+  while (i <= object.productsNumber) {
+    price = prompt(`Introduzca el precio del producto ${i}`);
+    priceProduct.push(price);
+    i++;
+  }
   object.discount = prompt("Introduzca algún texto", 20);
   object.paymentMethod = prompt("Introduzca algún texto", "Paypal");
   var discount = object.discount / 100;
 
   if (object.discount > 0) {
-    price = object.productsNumber * 10 - (object.productsNumber * 10 * discount);
+    totalPrice = object.productsNumber * 10 - (object.productsNumber * 10 * discount);
   } else {
-    price = object.productsNumber * 10;
+    totalPrice = object.productsNumber * 10;
   }
 
-  document.write(`Usted tiene que pagar ${price} euros`);
+  document.write(`Usted tiene que pagar ${totalPrice} euros`);
 }
