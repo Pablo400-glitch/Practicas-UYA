ca92488c30bb136752c8c39972ed2c1955134036
Pablo Pais Ficret <64638993+Pablo400@users.noreply.github.com>
2022-04-01 13:12:24 +0100

    Cambios

--- Práctica-4/code.js
+++ Práctica-4/code.js
@@ -12,25 +12,27 @@ function calculateMoney() {
   var totalPrice = 0;
   var price = 0;
   var i = 1;
-  var priceProduct = [];
+  var priceProduct = 0;
   object.dni = prompt("Introduzca su DNI", "XXXXXXXXX");
   object.birthday = prompt("Introduzca su cumpleaños", "1/1/22");
   object.accountNumber = prompt("Introduzca su número de cuenta", "XXXX-XXXX-XXXX-XXXX");
-  object.productsNumber = prompt("Introduzca algún texto", 0);
+  object.productsNumber = prompt("Introduzca el número de productos", 0);
 
   while (i <= object.productsNumber) {
     price = prompt(`Introduzca el precio del producto ${i}`);
-    priceProduct.push(price);
+    priceProduct += parseInt(price);
     i++;
   }
-  object.discount = prompt("Introduzca algún texto", 20);
-  object.paymentMethod = prompt("Introduzca algún texto", "Paypal");
+
+  object.discount = prompt("Introduzca un valor de descuento que tenga disponible", 0);
   var discount = object.discount / 100;
 
+  object.paymentMethod = prompt("Introduzca un metodo de pago", "Paypal");
+  
   if (object.discount > 0) {
-    totalPrice = object.productsNumber * 10 - (object.productsNumber * 10 * discount);
+    totalPrice = priceProduct - (priceProduct * discount);
   } else {
-    totalPrice = object.productsNumber * 10;
+    totalPrice = priceProduct;
   }
 
   document.write(`Usted tiene que pagar ${totalPrice} euros`);
