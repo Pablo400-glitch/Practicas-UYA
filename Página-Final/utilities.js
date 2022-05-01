42cd6f69a70c2850e709cae9a87fe7840a356f17
Pablo400 <alu0101311135@ull.edu.es>
2022-05-01 19:07:07 +0100

    Jquery Implementado Correctamente

--- Página-Final/utilities.js
+++ Página-Final/utilities.js
@@ -2,13 +2,26 @@ $(document).ready(function(){
   $("#MainCategories").hide();
   $("#MyAccount").hide();
 
+  // Main Categories and My Account nav
+
   $("#MainCategoriesTitle").mouseenter(function(){
+    $("#MainCategories").show();
+  });
+
+  // Accesibility Option
+  $("#MainCategoriesTitle").keyup(function(){
     $("#MainCategories").show();
   });
 
   $("#MyAccountTitle").mouseenter(function(){
     $("#MyAccount").show();
   });
+
+  // Accesibility Option
+  $("#MyAccountTitle").keyup(function(){
+    $("#MyAccount").show();
+  });
+
 
   $("#SecondMenu").mouseleave(function(){
     $("#MainCategories").hide();
