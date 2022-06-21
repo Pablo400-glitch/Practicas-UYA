a19ebbddf68883d865e06842529e3f4bd3a49aea
Pablo400 <alu0101311135@ull.edu.es>
2022-06-22 00:00:40 +0100

    Pruebas firebase

---
+++ Página-Final/login-register/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
+import { addDoc, getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-firestore.js"
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBZ4EK9cDvyU9gIY_MauxS-Mos8CjIN96M",
+  authDomain: "practica-final-uya.firebaseapp.com",
+  projectId: "practica-final-uya",
+  storageBucket: "practica-final-uya.appspot.com",
+  messagingSenderId: "30585017351",
+  appId: "1:30585017351:web:1a1707ff514bc72536c6a2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const saveTask = (title, description) => addDoc(collection(db, 'tasks'), {title, description})
