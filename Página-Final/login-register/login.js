81eba58ea860708332c957f6c4aea09ed62cbc54
Pablo400 <alu0101311135@ull.edu.es>
2022-06-22 19:18:32 +0100

    Cambio menor

--- Página-Final/login-register/login.js
+++ Página-Final/login-register/login.js
@@ -50,50 +50,5 @@ async function checkData() {
   }
 }
 
-// function checkData() {
-//   const xhttp = new XMLHttpRequest()
-//   xhttp.open('GET', 'catalogo.json', true)
-//   xhttp.send()
-
-//   xhttp.onreadystatechange = function() {
-//     if(this.readyState == 4 && this.status == 200) {
-//       let datos = JSON.parse(this.responseText);
-//       let password = document.getElementsByName('password')[0].value;
-//     	let nombre = document.getElementsByName('nombre')[0].value;
-//       let isUser = false;
-//       let empty = false;
-
-//       if (password === ' ' && nombre === ' ') {
-//         empty = true;
-//       } else {
-//         for (let item of datos) { 
-//           console.log(password);
-//           if(nombre === item.nombre && password === item.password) {    
-//             isUser = true;
-//           } 
-//         }
-//       }
-
-//       if (isUser === true) {
-//         alert('Se ha iniciado sesión de forma satisfactoria');
-//         $(document).ready(function(){
-//           $("#showdata").show();
-//           $("#login").hide();
-//           $("#nombretext").hide();
-//           $("#passwordtext").hide();
-//           $("#nombre").hide();
-//           $("#password").hide();
-//         });
-//       } else {
-//         if (empty === true) {
-//           alert('No ha introducido ningún dato');
-//         } else {
-//           alert('Esa cuenta no existe');
-//         }
-//       }
-//     }
-//   }
-// } 
-
 
 
